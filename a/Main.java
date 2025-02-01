81c605171c4c688a1d67060e22dc4ea597c3233d
JuanOlay <felipe.guevara.o.1211@gmail.com>
2025-01-31 19:19:28 -0500

    Inicio Test prueba subida de archivos a repo c:

---
+++ a/Main.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+        // Muestra un cuadro de diálogo con un mensaje
+        JOptionPane.showMessageDialog(null, "¡Hola, Mundo!", "Título del diálogo", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
